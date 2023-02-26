ab5fa6754f5e83c2eb8225ea5c7334501bee07e2
poorvaditya behre <aditya.behre@gmail.com>
2023-02-26 11:07:29 +0530

    updated Like model and added basic user model

--- src/models/like.js
+++ src/models/like.js
@@ -6,8 +6,24 @@ const likeSchema = new mongoose.Schema(
     onModel: {
       type: String,
       required: true,
-      enum: ["Tweet", "Comment"],
+      enum: ["Tweet", "Comment"], // either like on tweet or comment if we include stories then we can increase this enum
     },
+    likeable: {
+      // whether you have liked a tweet or comment based on that we can store respective id's
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      refPath: "onModel", //refPath
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    
   },
   { timestamps: true }
 );
+
+const Like = mongoose.model("Like", likeSchema);
+
+export default Like;
---
+++ src/models/user.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
